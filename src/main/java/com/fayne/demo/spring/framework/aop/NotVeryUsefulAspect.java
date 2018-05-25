c0c1f25fe57b59e08f1d4616cb9d7266f9bbf8e8
Wangzezhou(301255) <83209400@qq.com>
2018-05-25 17:56:41 +0800

    Add AOP demo 10%

--- src/main/java/com/fayne/demo/spring/framework/aop/NotVeryUsefulAspect.java
+++ src/main/java/com/fayne/demo/spring/framework/aop/NotVeryUsefulAspect.java
@@ -1,8 +1,15 @@
 package com.fayne.demo.spring.framework.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
 /**
  * @Author: WangZezhou(301255)
  * @Date 2018/5/24 18:39
  */
+@Aspect
 public class NotVeryUsefulAspect {
+
+    @Pointcut("")
+    private void anyOldTransfer(){}
 }
